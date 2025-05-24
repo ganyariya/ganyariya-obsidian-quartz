192cf9b14a2778d25175daafb43a12e9b6d5aef1
ganyariya <ganariya2525@gmail.com>
2025-05-24 11:18:22 +0900

    change footer url

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,8 +8,8 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      "Blog": "https://blog.ganyariya.dev",
+      "GitHub": "https://github.com/ganyariya",
     },
   }),
 }
